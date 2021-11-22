af7950cd2c76ef3048975c163e4d80e82f688d33
Akpagni5547 <85566432+Akpagni5547@users.noreply.github.com>
2021-11-22 01:49:56 +0000

    Initial commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,17 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import UserValidator from 'App/Validators/UserValidator'
+
+export default class AuthController {
+  public async register({ request, response }: HttpContextContract) {
+    const payload = await request.validate(UserValidator)
+    await User.create(payload)
+    response.redirect('/login')
+  }
+
+  public async login({ request, response, auth }: HttpContextContract) {
+    const users = request.only(['email', 'password'])
+    await auth.attempt(users.email, users.password)
+    response.redirect('/')
+  }
+}
